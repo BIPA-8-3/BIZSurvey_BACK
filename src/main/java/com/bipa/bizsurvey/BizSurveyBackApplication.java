d47e1a5624b5e08b25c214293a2f988c6a26b404
WonsikHwang <j2s0007@gmail.com>
2023-11-03 16:03:39 +0900

    base commit

---
+++ src/main/java/com/bipa/bizsurvey/BizSurveyBackApplication.java
@@ -0,0 +1,13 @@
+package com.bipa.bizsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizSurveyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizSurveyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bipa/bizsurvey/BizSurveyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bipa.bizsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizSurveyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
