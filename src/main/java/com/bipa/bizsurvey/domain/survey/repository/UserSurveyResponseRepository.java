3fc8ba508901090995e150d7cf360a09eb07afb6
tud0 <psy563012@gmail.com>
2023-11-23 11:26:24 +0900

    [Refactor] : import 제거

--- src/main/java/com/bipa/bizsurvey/domain/survey/repository/UserSurveyResponseRepository.java
+++ src/main/java/com/bipa/bizsurvey/domain/survey/repository/UserSurveyResponseRepository.java
@@ -2,13 +2,11 @@ package com.bipa.bizsurvey.domain.survey.repository;
 
 import com.bipa.bizsurvey.domain.community.domain.SurveyPost;
 import com.bipa.bizsurvey.domain.survey.domain.UserSurveyResponse;
-import com.bipa.bizsurvey.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Map;
 
 public interface UserSurveyResponseRepository extends JpaRepository<UserSurveyResponse, Long> {
 
