facf45f9f3b50c2ea009e07e3f7280e2e034ab80
tud0 <psy563012@gmail.com>
2023-11-27 20:03:53 +0900

    [Feat] : 설문 결과 dto추가

---
+++ src/main/java/com/bipa/bizsurvey/domain/survey/dto/response/SurveyResultResponse.java
@@ -0,0 +1,18 @@
+package com.bipa.bizsurvey.domain.survey.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SurveyResultResponse {
+
+    List<String> answerUsers;
+
+    List<AnswerResultResponse> answerResponses;
+
+}
